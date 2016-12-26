ca1e105b090566c5ebb03fcf9a70ce626fb78508
Stargator <wildbug@linuxmail.org>
2016-12-25 21:56:59 -0500

    Address analysis issues

--- src/main/java/test/net/util/Log4j2Example.java
+++ src/main/java/test/net/util/Log4j2Example.java
@@ -2,22 +2,23 @@ package test.net.util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-//import org.apache.logging.log4j.core.config.XMLConfiguration;
 
 /**
  *
  * @author Stargator
  */
 public class Log4j2Example {
-	static final Logger logger = LogManager.getLogger(Log4j2Example.class.getName());
+    static final Logger logger = LogManager.getLogger(Log4j2Example.class.getName());
 
-        public static void main(String[] args) {
+    private Log4j2Example() {}
 
-		logger.trace("Entering Log4j Example.");
-		Hello hello = new Hello();
-		if (!hello.callMe()) {
-			logger.error("Failing Now!");
-		}
-		logger.trace("Exiting Log4j Example.");
-	}
+    public static void main(String[] args) {
+
+        logger.trace("Entering Log4j Example.");
+        Hello hello = new Hello();
+        if (!hello.callMe()) {
+            logger.error("Failing Now!");
+        }
+        logger.trace("Exiting Log4j Example.");
+    }
 }
